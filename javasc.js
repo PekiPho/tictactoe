4bd728d69418e79c94b3331c5efcfbf194132cf8
PekiPho <v.a.r.d.games123@gmail.com>
2024-02-06 16:58:31 +0100

    made winning conditions work

--- javasc.js
+++ javasc.js
@@ -1,8 +1,13 @@
-let row1=document.querySelector('.r1').children;
-let row2=document.querySelector('.r2').children;
-let row3=document.querySelector('.r3').children;
+let row=[]
+row[0]=document.querySelector('.r1').children;
+row[1]=document.querySelector('.r2').children;
+row[2]=document.querySelector('.r3').children;
 
 let z=1;
+let matrix = [];
+for(var i=0; i<3; i++) {
+    matrix[i] = new Array(3).fill(0);
+}
 
 let play=document.querySelectorAll('.play');
 play.forEach((e)=>{
@@ -14,12 +19,44 @@ play.forEach((e)=>{
         else{
             e.textContent='O';
             e.style.color='rgba(10,72,227,1)';
+            e.id=String(-1*e.id);
         } 
         e.classList.add('disabledDiv');
         z=-z;
+        checkWin();
     });
 });
+function getValues(){
+    for(let i=0;i<3;i++){
+        for(let j=0;j<3;j++)
+        {
+            if(row[i][j].textContent!=='')
+                matrix[i][j]=Number(row[i][j].id);
+        }
+    }
+}
+function resetGame(a){
+    if(a===15)
+        console.log('X wins');
+    else console.log('O wins');
+}
 
 function checkWin(){
-
+    getValues();
+    let s=new Array(8).fill(0);
+    for(let i=0;i<3;i++){
+        s[0]+=matrix[i][i];
+        s[1]+=matrix[0][i];
+        s[2]+=matrix[1][i];
+        s[3]+=matrix[2][i];
+        s[4]+=matrix[i][0];
+        s[5]+=matrix[i][1];
+        s[6]+=matrix[i][2];
+        s[7]+=matrix[2-i][i];
+    }
+    for(let i=0;i<8;i++)
+    {
+        if(Math.abs(s[i])===15)
+            resetGame(s[i]);
+    }
 }
